c2184aa445493d674bc94ee2c53418fe06f82cd5
TrelawneyJourney <the-trelawney-journey@outlook.com>
2025-04-07 22:16:47 -0300

    comienzo collaboration section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ButtonGradient from "./assets/svg/ButtonGradient";
 import Button from "./components/Button";
 import Hero from "./components/Hero";
 import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Header />
         <Hero />
         <Benefits />
+        <Collaboration />
       </div>
       <ButtonGradient />
     </>
---
+++ src/components/Collaboration.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Section from "./Section";
+import { collabContent } from "../constants";
+
+const Collaboration = () => {
+  return (
+    <Section crosses>
+      <div className="container lg:flex">
+        <div className="max-w-[25rem]">
+          <h2 className="h2 mb-4 md:mb-8">
+            AI Chat App for seamless collaboration
+          </h2>
+          <ul className="max-w-[22rem] mb-10 md:mb-14">
+            {collabContent.map((item) => (
+              <li key={item.id} className="mb-3 py-3">
+                {item.title}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </Section>
+  );
+};
+
+export default Collaboration;
