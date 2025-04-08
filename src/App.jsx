00de15246ac9ce0bc40e8636f1970e1b086126b7
TrelawneyJourney <the-trelawney-journey@outlook.com>
2025-04-08 12:37:49 -0300

    services section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Button from "./components/Button";
 import Hero from "./components/Hero";
 import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
+import Services from "./components/Services";
 
 const App = () => {
   return (
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import Section from "./Section";
+import Heading from "./Heading";
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <Section id="how-to-use">
+      <div className="container">
+        <Heading
+          title="Generative AI made for creators."
+          text="Brainwave unlocks the potential of AI-powered applications"
+        />
+      </div>
+    </Section>
+  );
 };
 
 export default Services;
