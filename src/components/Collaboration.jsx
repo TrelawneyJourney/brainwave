449ca4d760967b00e14c70f3f4280958b29b5d93
TrelawneyJourney <the-trelawney-journey@outlook.com>
2025-04-08 12:18:56 -0300

    end collaboration section

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Section from "./Section";
-import { collabContent } from "../constants";
+import { collabApps, collabContent, collabText } from "../constants";
+import { brainwaveSymbol, check } from "../assets";
+import Button from "./Button";
+import { LeftCurve, RightCurve } from "./design/Collaboration";
 
 const Collaboration = () => {
   return (
-    <Section crosses>
+    <Section crosses customPaddings={"lg:py-10 xl:py-16"}>
       <div className="container lg:flex">
         <div className="max-w-[25rem]">
           <h2 className="h2 mb-4 md:mb-8">
@@ -13,10 +16,72 @@ const Collaboration = () => {
           <ul className="max-w-[22rem] mb-10 md:mb-14">
             {collabContent.map((item) => (
               <li key={item.id} className="mb-3 py-3">
-                {item.title}
+                <div className="flex items-center">
+                  <img src={check} width={24} height={24} alt="check" />
+                  <h6 className="body-2 ml-5">{item.title}</h6>
+                </div>
+                {item.text && (
+                  <p className="body-2 mt-3 text-n-4">{item.text}</p>
+                )}
               </li>
             ))}
           </ul>
+
+          <Button> Try it now</Button>
+        </div>
+        <div className="lg:ml-auto xl:w-[45rem] mt-4">
+          <p className="body-2 mb-8 text-n-4 md:mb-16 lg:mb-32 lg:w-[22rem] lg:mx-auto">
+            {collabText}
+          </p>
+
+          {/* wrapper for ilustration 
+          div- primer circulo
+          */}
+          <div className="relative left-1/2 flex w-[22rem] aspect-square border border-n-6 rounded-full -translate-x-1/2 scale:75 md:scale-100">
+            {/*segundo circulo */}
+            <div className="flex w-60 aspect-square m-auto border border-n-6 rounded-full">
+              {/**tercer circulo */}
+              <div className="w-[6rem] aspect-square m-auto p-[0.2rem] bg-conic-gradient rounded-full">
+                {/*centro img */}
+                <div className="flex items-center justify-center w-full h-full bg-n-8 rounded-full">
+                  <img
+                    src={brainwaveSymbol}
+                    width={48}
+                    height={48}
+                    alt="brainwave"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <ul>
+              {/*-ml-[1.6rem] position it on a specific angle*/}
+              {collabApps.map((app, index) => (
+                <li
+                  key={app.id}
+                  className={`absolute top-0 left-1/2 h-1/2 -ml-[1.6rem] origin-bottom rotate-${
+                    index * 45
+                  }`}
+                >
+                  <div
+                    className={`relative -top-[1.6rem] flex w-[3.2rem] h-[3.2rem] bg-n-7 border border-n-1/15 rounded-xl -rotate-${
+                      index * 45
+                    }`}
+                  >
+                    <img
+                      src={app.icon}
+                      className="m-auto"
+                      width={app.width}
+                      height={app.height}
+                      alt={app.title}
+                    />
+                  </div>
+                </li>
+              ))}
+            </ul>
+            <LeftCurve />
+            <RightCurve />
+          </div>
         </div>
       </div>
     </Section>
