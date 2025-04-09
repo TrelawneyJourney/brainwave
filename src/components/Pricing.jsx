6cc226700fa489f3f3c52d74f69fbf45a4797120
TrelawneyJourney <the-trelawney-journey@outlook.com>
2025-04-08 22:27:22 -0300

    pricing section and heading modifications

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import Section from "./Section";
+import { smallSphere, stars } from "../assets";
+import Heading from "./Heading";
 
 const Pricing = () => {
-  return <div>Pricing</div>;
+  return (
+    <Section className="overflow-hidden" id="pricing">
+      <div className="container relative z-2">
+        {/**sphere and stars*/}
+        <div className="hidden relative justify-center mb-[6.5rem] lg:flex">
+          <img
+            src={smallSphere}
+            alt="Sphere"
+            className="relative z-1"
+            width={255}
+            height={255}
+          />
+          {/**starts */}
+          <div className="absolute top-1/2 left-1/2 w-[60rem] -translate-y-1/2 -translate-x-1/2 pointer-events-none">
+            <img
+              src={stars}
+              className="w-full"
+              width={950}
+              height={400}
+              alt="Stars"
+            />
+          </div>
+        </div>
+
+        <Heading
+          tag="Get started with Brainwave"
+          title="Pay once, use forever"
+        />
+
+        {/**pricing list */}
+        <div className="relative"></div>
+      </div>
+    </Section>
+  );
 };
 
 export default Pricing;
